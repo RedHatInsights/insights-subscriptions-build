4e548001bbc3e3f536672530944a44b54b25921f
Travis CI User <travis@example.org>
2018-10-17 17:52:11 +0000

    Build by Travis

---
+++ js/Rules.js
@@ -0,0 +1,31 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["Rules"],{
+
+/***/ "./src/PresentationalComponents/Rules/ListRules.js":
+/*!*********************************************************!*\
+  !*** ./src/PresentationalComponents/Rules/ListRules.js ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @red-hat-insights/insights-frontend-components */ "./node_modules/@red-hat-insights/insights-frontend-components/index.js");
+/* harmony import */ var _red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+var ListRules = function ListRules() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1__["PageHeader"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1__["PageHeaderTitle"], {
+    title: "Rules Page"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, " The is the route to the rules page ")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_red_hat_insights_insights_frontend_components__WEBPACK_IMPORTED_MODULE_1__["Main"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, " Content ")));
+};
+
+ListRules.displayName = 'list-rules';
+/* harmony default export */ __webpack_exports__["default"] = (ListRules);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../sourcemaps/Rules.js.map
